6a543b8f07225383f0b94a4b7739335a55239018
LucasPires50 <luc_50@outlook.com>
2023-12-15 11:46:00 -0300

    feat: script to run the project

---
+++ script.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+docker ps | grep postgres_django >/dev/null 2>&1
+if [ $? -eq 0 ]; then
+    echo "Container postgres_django já está em execução!"
+    # Adicione o comando desejado aqui
+else
+    docker container run -d -p 5533:5432 \
+        --name postgres_django \
+        --mount type=volume,src=teste1,dst=/data \
+        -e POSTGRES_USER=docker \
+        -e POSTGRES_PASSWORD=docker123 \
+        -e POSTGRES_DB=docker \
+        postgres
+
+
+    echo "Script executado com sucesso!"
+fi
+
+poetry run python manage.py runserver
