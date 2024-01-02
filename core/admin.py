303e18e64af5b95ae27cbe713c699059e2161dd8
LucasPires50 <luc_50@outlook.com>
2024-01-02 14:22:17 -0300

    feat: config admin

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Cargo, Servico, Equipe
+
+@admin.register(Cargo)
+class CargoAdmin(admin.ModelAdmin):
+    list_display = ('cargo', 'ativo', 'modificado')
+
+@admin.register(Servico)
+class ServicoAdmin(admin.ModelAdmin):
+    list_display = ('servico', 'icone', 'ativo', 'modificado')
+
+@admin.register(Equipe)
+class EquipeAdmin(admin.ModelAdmin):
+    list_display = ('nome', 'cargo', 'ativo', 'modificado')
+
--- core/models.py
+++ core/models.py
@@ -1,5 +1,12 @@
+import uuid
 from django.db import models
 from stdimage.models import StdImageField
+
+def get_file_path(instance, filename):
+    # mudar o nome dos arquivos salvos, para uma chave aleátoria
+    ext = filename.split('.')[-1]
+    filename = f'{uuid.uuid4()}.{ext}'
+    return filename
 
 class Base(models.Model):
     criados = models.DateField('Criação', auto_now_add=True)
@@ -39,11 +46,11 @@ class Cargo(Base):
     def __str__(self):
         return self.cargo
 
-class equipe(Base):
+class Equipe(Base):
     nome = models.CharField('Nome', max_length=100)
     cargo = models.ForeignKey("core.Cargo", verbose_name='Cargo', on_delete=models.CASCADE)
     bio = models.TextField('Bio', max_length=200)
-    image = StdImageField('Imagens', upload_to='equipe', variations={'thumb': {'width':480, 'height':480, 'crop': True}})
+    image = StdImageField('Imagens', upload_to=get_file_path, variations={'thumb': {'width':480, 'height':480, 'crop': True}})
     facebook = models.CharField('Facebook', max_length=100, default='#')
     twitter = models.CharField('Twitter', max_length=100, default='#')
     instagram = models.CharField('Instagram', max_length=100, default='#')
