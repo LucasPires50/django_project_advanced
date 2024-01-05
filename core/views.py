9ee96d6fe2d3f71b2c988ebe2fff97b2acb0beb8
LucasPires50 <luc_50@outlook.com>
2024-01-05 16:34:49 -0300

    feat: config contact and send email

--- core/views.py
+++ core/views.py
@@ -1,10 +1,15 @@
-from typing import Any
-from django.views.generic import TemplateView
+
+from django.views.generic import FormView, TemplateView
+from django.urls import reverse_lazy
+from django.contrib import messages
 
 from .models import Servico, Equipe, Recurso
+from .forms import ContatoForm
 
-class IndexView(TemplateView):
-    template_name = "index.html"
+class IndexView(FormView):
+    template_name = 'index.html'
+    form_class = ContatoForm
+    success_url = reverse_lazy('index')
     
     # Metodo para recuperar o context da pagina "index.html"
     def get_context_data(self, **kwargs):
@@ -15,6 +20,16 @@ class IndexView(TemplateView):
         context['recursos'] = Recurso.objects.order_by('?').all()
         
         return context
+    
+    def form_valid(self, form, *args, **kwargs):
+        form.send_mail()
+        messages.success(self.request, 'E-mail enviado com sucesso')
+        return super(IndexView, self).form_valid(form, *args, **kwargs)
+
+    def form_invalid(self, form, *args, **kwargs):
+        messages.error(self.request, 'Erro ao enviar e-mail')
+        return super(IndexView, self).form_invalid(form, *args, **kwargs)
+    
         
 
 class TestView(TemplateView):
