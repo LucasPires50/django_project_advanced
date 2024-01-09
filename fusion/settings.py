41e21ad87b7272673315e36909c73a28b7492559
LucasPires50 <luc_50@outlook.com>
2024-01-09 15:55:41 -0300

    feat: config

--- fusion/settings.py
+++ fusion/settings.py
@@ -45,7 +45,6 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    # 'whitenoise.runserver_nostatic'
     'django.contrib.staticfiles',
     'core',
 ]
@@ -146,9 +145,6 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-# STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static/')]
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
-
 
 MEDIA_URL = 'media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
