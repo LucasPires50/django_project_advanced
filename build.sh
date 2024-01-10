aaa89f710889b02b7bcd761d6e40edaeae213261
LucasPires50 <luc_50@outlook.com>
2024-01-10 16:33:52 -0300

    feat: rande.yaml

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@ set -o errexit
 
 export PATH="/opt/render/project/poetry/bin:$PATH"
 
-poetry install
+poetry install --no-root
 
 poetry run python manage.py collectstatic --no-input
 poetry run python manage.py migrate
