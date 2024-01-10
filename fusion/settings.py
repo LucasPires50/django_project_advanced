a9ea8e50c25fc4837d284c1baacd4233a0d38afd
LucasPires50 <luc_50@outlook.com>
2024-01-10 16:55:59 -0300

    feat: config

--- fusion/settings.py
+++ fusion/settings.py
@@ -30,12 +30,8 @@ SECRET_KEY = os.environ.get('SECRET_KEY', config('SECRET_KEY'))
 # DEBUG = os.environ.get('DEBUG', "False").lower == "true"
 
 DEBUG = os.environ.get('DEBUG', config('DEBUG'))
-print(DEBUG)
-ALLOWED_HOSTS = []
 
-RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
-if RENDER_EXTERNAL_HOSTNAME:
-    ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', ['*'])
 
 
 # Application definition
